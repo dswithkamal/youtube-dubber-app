45bcee893b82579d4ff555fe2168ba1475e73fcd
Kamal Sharma <128659969+dswithkamal@users.noreply.github.com>
2025-06-30 16:20:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
 
-# Update package list
-sudo apt-get update
-
-# Install system dependencies
-sudo apt-get install -y ffmpeg python3-pip python3-venv
-
-# Create virtual environment
-python3 -m venv venv
-source venv/bin/activate
-
-# Install Python packages
-pip install --upgrade pip
-pip install -r requirements.txt
